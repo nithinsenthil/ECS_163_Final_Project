23304f3bf9b7db3074dea8cc1f378353304ac980
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-05 21:23:02 -0700

    feat: Approximately size bar, start sizing others, and reformat styles

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ const margins = {
 // Title Height
 const headerHeight = 100;
 
+/**
+ * Given a JavaScript selector, target its wrapper and use the wrapper to calculate the chart dimensions
+ * 
+ * @param {string} selector 
+ * @param {{top: number; right: number; bottom: number; left: number}} margins Object with
+ *                          the numerical values of margins to use for the chart visualization
+ * @returns 
+ */
+function getWrapperDims(selector, margins) {
+  const wrapper = document.querySelector(selector).closest(".visWrapper");
+  const { width, height } = wrapper.getBoundingClientRect();
+  return {
+    width,
+    height,
+    innerWidth: width - margins.left - margins.right,
+    innerHeight: height - margins.top - margins.bottom,
+  };
+}
+
 // Dimensions for all three charts
 const chartDims = {
   chloropleth: {
@@ -21,12 +40,7 @@ const chartDims = {
     innerWidth: width - margins.left - margins.right,
     innerHeight: height - headerHeight - margins.top - margins.bottom,
   },
-  bar: {
-    width: width,
-    height: height,
-    innerWidth: width - margins.left - margins.right,
-    innerHeight: height - headerHeight - margins.top - margins.bottom,
-  },
+  bar: getWrapperDims(".chart", margins),
   scatter: {
     width: width,
     height: height,
