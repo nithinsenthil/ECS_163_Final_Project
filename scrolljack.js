c666b9f4efd65d67ff3365d066675db8118a3efb
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-07 13:05:55 -0700

    feat: Add basic scrolljack flow

---
+++ scrolljack.js
@@ -0,0 +1,27 @@
+function scaleOpacity(value) {
+  if (value >= 0.7) {
+    return 1;
+  }
+  if (value <= 0.3) {
+    return 0;
+  }
+  return (value - 0.2) / 0.4;
+}
+
+window.addEventListener("scroll", function () {
+  const num = window.scrollY / window.innerHeight;
+  console.log("scroll", num);
+  let x = 0;
+
+  console.log(document.getElementsByClassName("section"));
+
+  [...document.getElementsByClassName("section")].forEach((element) => {
+    element.style.opacity = 0;
+  });
+
+  for (let target of Array.from({ length: 5 }, (_, idx) => idx)) {
+    const element = document.getElementById(`section-${target + 1}`);
+    const dist = Math.abs(target - num);
+    element.style.opacity = scaleOpacity(1 - 2 * dist);
+  }
+});
