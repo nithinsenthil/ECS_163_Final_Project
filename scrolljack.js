c273bd3d74461194ae64ad89df0d8b80488db78e
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-07 13:37:05 -0700

    feat: Use CSS animations to animate position effect and replace opacity scale with opacity animations

--- scrolljack.js
+++ scrolljack.js
@@ -1,24 +1,19 @@
-function scaleOpacity(value) {
-  if (value >= 0.4) {
-    return 1;
-  }
-  if (value <= 0.2) {
-    return 0;
-  }
-  return (value - 0.2) / 0.2;
-}
-
 window.addEventListener("scroll", function () {
   const num = window.scrollY / window.innerHeight;
   let x = 0;
 
-  [...document.getElementsByClassName("section")].forEach((element) => {
-    element.style.opacity = 0;
-  });
-
   for (let target of Array.from({ length: 5 }, (_, idx) => idx)) {
     const element = document.getElementById(`section-${target + 1}`);
-    const dist = Math.abs(target - num);
-    element.style.opacity = scaleOpacity(1 - 2 * dist);
+    const dist = target - 4 * num;
+    if (dist > 0.5) {
+      element.style.opacity = "0";
+      element.style.transform = "translateY(100vh)";
+    } else if (dist < -0.5) {
+      element.style.opacity = "0";
+      element.style.transform = "translateY(-100vh)";
+    } else {
+      element.style.opacity = "1";
+      element.style.transform = "translateY(0)";
+    }
   }
 });
